4cfd8af4a0d403c70025f81c92648631bf37fb31
suyogly <bizpurpose09@gmail.com>
2025-12-26 15:09:06 +0545

    feat: added web searching tool with tavily

--- agents/3.py
+++ agents/3.py
@@ -1,2 +1,23 @@
 from langchain_groq import ChatGroq
 from langchain.agents import create_agent
+from langchain_tavily import TavilySearch
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intent = input("What do you want to know?")
+
+search_tool = TavilySearch(
+    max_results = 1
+)
+
+llm = ChatGroq(model="openai/gpt-oss-120b", temperature=0.7)
+
+agent = create_agent(
+    model=llm,
+    system_prompt="you are the helpful assistant.",
+    tools=[search_tool]
+)
+
+res = search_tool.invoke({"query": intent})
+print(res['results'][0]['content'])
