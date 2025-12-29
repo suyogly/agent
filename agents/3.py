935f8e7d4f31a5b4cb16073b27d7e0d2405bdf66
suyogly <bizpurpose09@gmail.com>
2025-12-29 12:57:04 +0545

    feat: first tool call using llm with own weather logic

--- agents/3.py
+++ agents/3.py
@@ -1,23 +1,41 @@
 from langchain_groq import ChatGroq
 from langchain.agents import create_agent
 from langchain_tavily import TavilySearch
+from weather import get_weather_info
+from langchain.tools import tool
 from dotenv import load_dotenv
 
 load_dotenv()
+def get_city():
+    name = input("Enter the name of the city: ")
+    return name
 
-intent = input("What do you want to know?")
+@tool("weather")
+def weather_tool():
+    '''
+    Searches weather for the specified city.
+    '''
+    res = get_weather_info(name=get_city())
+    return res
 
-search_tool = TavilySearch(
-    max_results = 1
+model = ChatGroq(
+    model="openai/gpt-oss-120b",
+    temperature=0.2
 )
-
-llm = ChatGroq(model="openai/gpt-oss-120b", temperature=0.7)
 
 agent = create_agent(
-    model=llm,
-    system_prompt="you are the helpful assistant.",
-    tools=[search_tool]
+        model=model,
+        system_prompt="you are a helpful, but only answers in long when necessary.",
+        tools=[weather_tool]
+    )
+
+res = agent.invoke(
+    {
+            "messages": [
+                {"role": "user",
+                "content": get_city()}
+            ]
+        }
 )
 
-res = search_tool.invoke({"query": intent})
-print(res['results'][0]['content'])
+print(res["messages"][-1].content)
